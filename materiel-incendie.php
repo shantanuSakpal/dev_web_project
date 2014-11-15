ada5c1d7176af83ba4256dfa92ec0d5915af41f2
Louis <louis.moreau@hei.fr>
2014-11-15 19:42:31 +0100

    add login form

--- materiel-incendie.php
+++ materiel-incendie.php
@@ -18,7 +18,6 @@ include "header.php";?>
           <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque ultricies fermentum. In hac habitasse platea dictumst. Donec accumsan magna leo, a facilisis lectus iaculis a. Fusce mattis libero ac enim efficitur efficitur. Vivamus et lorem condimentum, consequat felis ut, volutpat justo. Aliquam sed mollis magna. Mauris tristique eros nec purus cursus convallis. Sed fermentum erat sed arcu tincidunt viverra in a erat. Sed risus dui, gravida mollis risus sed, aliquet blandit dolor.
           </p>
-
           <p>
               Integer elementum, odio at auctor maximus, orci orci consectetur dui, et vestibulum diam magna ac massa. Suspendisse justo risus, malesuada id turpis rutrum, blandit dignissim magna. Curabitur pretium, felis eget ornare aliquet, lacus dui elementum est, eu lobortis ante magna vitae sapien. Integer vel venenatis eros. Maecenas eget turpis tincidunt, feugiat ante a, tincidunt nibh. Nulla congue rhoncus mauris, vel mattis urna venenatis vel. Sed ullamcorper pellentesque orci, dignissim vulputate ante condimentum ut.
           </p>
@@ -27,12 +26,23 @@ include "header.php";?>
 </div>
 <div class="row">
     <div class="shell12">
-        <div class="shell10">
-            <div class="shell4 product">
+        <div class="shell2 cart">
+            <div class="shell2">
+                <i class="fa fa-2x fa-align-center fa-shopping-cart"></i>
+            </div>
+            <div class="shell10">
+                <h2 class="center">Devis</h2>
+            </div>
+            <div class="shell12">
+                <p>Récapitulatif de devis :</p>
+            </div>
+        </div>
+        <div class="shell9">
+            <div id="product" class="shell4 product">
                 <h4>Lance à incendie</h4>
                 <img src="img/fr-default-home_default.jpg">
                 <p>Catégories: Lance, CO2</p>
-                <p>Détail<p>
+                <p>Détails</p>
                 <p>Demander un devis</p>
             </div>
             <div class="shell4 product">
@@ -71,17 +81,7 @@ include "header.php";?>
                 <p>Demander un devis</p>
             </div>
         </div>
-        <div class="shell2 cart">
-            <div class="shell2">
-                <i class="fa fa-2x fa-align-center fa-shopping-cart"></i>
-            </div>
-            <div class="shell10">
-                <h2 class="center">Devis</h2>
-            </div>
-            <div class="shell12">
 
-            </div>
-        </div>
     </div>
 </div>
 
--- requalification.php
+++ requalification.php
@@ -7,6 +7,30 @@
  */
 
 include "header.php";?>
+    <div class="row">
+        <div class="shell12 banner-mat-incendie">
+            <div class="shell8">
+                <h2 class="center">Requalification</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque ultricies fermentum. In hac habitasse platea dictumst. Donec accumsan magna leo, a facilisis lectus iaculis a. Fusce mattis libero ac enim efficitur efficitur. Vivamus et lorem condimentum, consequat felis ut, volutpat justo. Aliquam sed mollis magna. Mauris tristique eros nec purus cursus convallis. Sed fermentum erat sed arcu tincidunt viverra in a erat. Sed risus dui, gravida mollis risus sed, aliquet blandit dolor.
+                </p>
 
+                <p>
+                    Integer elementum, odio at auctor maximus, orci orci consectetur dui, et vestibulum diam magna ac massa. Suspendisse justo risus, malesuada id turpis rutrum, blandit dignissim magna. Curabitur pretium, felis eget ornare aliquet, lacus dui elementum est, eu lobortis ante magna vitae sapien. Integer vel venenatis eros. Maecenas eget turpis tincidunt, feugiat ante a, tincidunt nibh. Nulla congue rhoncus mauris, vel mattis urna venenatis vel. Sed ullamcorper pellentesque orci, dignissim vulputate ante condimentum ut.
+                </p>
+            </div>
+            <div class="shell4">
+                <img src="img/bouteilles-plongee.jpg" width="100%">
+            </div>
+        </div>
+    </div>
+<div class="row">
+    <div class="shell6">
+        <h4 class="center">Exincteur CO2</h4>
+    </div>
+    <div class="shell6">
+        <h4 class="center">Bouteille de plongée</h4>
+    </div>
+</div>
 
 <?php include "footer.php"?>
